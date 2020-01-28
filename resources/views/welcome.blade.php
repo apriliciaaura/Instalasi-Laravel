6807c7c891f28540ae5668a5cc8198db9455e089
apriliciaaura <aura.salsabilah1804@gmail.com>
2020-01-28 19:42:03 +0700

    Instalasi-Laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laradinus comunity</title>
+</head>
+<body background="img/bg.jpg">
+    <font face="Roboto" color="#3b3b3b">
+        
+    
+
+    <table width="100%" align="center" border="0" cellpadding="0" cellspacing="0" height="100%" background="">
+        <tboody>
+            <tr>
+                <td>
+                    <table width="1140px" border="0"  align="center">
+                        <tbody>
+                            <!-- Header -->
+                            <tr>
+                            
+                                <td align="right">
+                                    <b><a href="index.html" title="Home"><font face="Roboto" color="#fa6f49">Home</font></a></b> &nbsp;&nbsp;&nbsp;
+                                    <b><a href="portofolio.html" title="Portofolio"><font face="Roboto" color="#3b3b3b">Portofolio</font></a></b> &nbsp;&nbsp;&nbsp;
+                                    <b><a href="about.html" title="About Me"><font face="Roboto" color="#3b3b3b">About Us</font></a></b> &nbsp;&nbsp;&nbsp;
+                                    <b><a href="blog.html" title="Blog"><font face="Roboto" color="#3b3b3b">Blog</font></a></b> &nbsp;&nbsp;&nbsp;
+                                    <b><a href="contact.html" title="Contact Me"><font face="Roboto" color="#3b3b3b">Contact Us</font></a></b> &nbsp;&nbsp;&nbsp;
+                                    <b><a href="login.html" title="Login"><font face="Roboto" color="#3b3b3b">Login</font></a></b> &nbsp;&nbsp;&nbsp;
+                                </td>
+
+                            </tr>
+                            <tr>
+                                <td height="30px"  colspan="2"></td>
+                            </tr>
+                            <!-- Content -->
+                            <tr>
+                                <td colspan="2">
+                                <font size="30"><b>Welcome to<br> Laravel <font color="#fa6f49">Comunity.</font></b></font>
+                                <br>
+                                <br>
+                                <p>
+                                <b>==================================================================================================================================</b>
+                                <br>
+
+                                </p>
+                                </td>
+                            </tr>
+                            
+                            <tr>
+                                <td height="30px"  colspan="2"></td>
+                            </tr>
+                            
+                           <tr>
+                                <td colspan="2">
+                                <b>Laravel diluncurkan sejak tahun 2011 dan mengalami pertumbuhan yang cukup eksponensial. Di tahun 2015, Laravel adalah framework yang paling banyak mendapatkan bintang di Github. Sekarang framework ini menjadi salah satu yang populer di dunia, tidak terkecuali di Indonesia. 
+
+Laravel fokus di bagian end-user, yang berarti fokus pada kejelasan dan kesederhanaan, baik penulisan maupun tampilan, serta menghasilkan fungsionalitas aplikasi web yang bekerja sebagaimana mestinya. Hal ini membuat developer maupun perusahaan menggunakan framework ini untuk membangun apa pun, mulai dari proyek kecil hingga skala perusahaan kelas atas.
+
+Laravel mengubah pengembangan website menjadi lebih elegan, ekspresif, dan menyenangkan, sesuai dengan jargonnya “The PHP Framework For Web Artisans”. Selain itu, Laravel juga mempermudah proses pengembangan website dengan bantuan beberapa fitur unggulan, seperti Template Engine, Routing, dan Modularity.</font></b></font>
+                                <br>
+                                <br>
+                                <p>
+                                <b>==================================================================================================================================</b>
+                                <br>
+
+                                </p>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td height="30px"  colspan="2"></td>
+                            </tr>
+                            <!-- Footer -->
+
+
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+        </tboody>
+    </table>
+
+
+
+
+
+    </font>
+</body>
+</html>
